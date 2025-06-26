4c94c1cb211670ff8058b06b896c82d0347bbc00
Angel <amhernandez@techfactors.com>
2025-06-26 15:09:06 +0800

    Add base for Accounts page

--- src/pages/Accounts/Accounts.js
+++ src/pages/Accounts/Accounts.js
@@ -1,14 +1,48 @@
 import MainContainer from "../../components/MainContainer";
+import { dummyAccounts } from "./dummyData";
 
-function Accounts(){
+export default function Accounts(){
+
     return (
         <MainContainer>
+            <h1>Account Management</h1>
             <div>
-                <h1>Accounts Page</h1>
-                <p>Welcome to the Accounts page!</p>
+                <div>
+                    <button>All</button>
+                    <button>HR</button>
+                    <button>Employee</button>
+                </div>
+                <div>
+                    <div>Search Bar</div>
+                    <div>Filter</div>
+                </div>
             </div>
+            <table>
+                <thead>
+                    <th>ID</th>
+                    <th>Full Name</th>
+                    <th>Email</th>
+                    <th>Phone No.</th>
+                    <th>Username</th>
+                    <th>Department</th>
+                    <th>Role</th>
+                </thead>
+                <tbody>
+                     {
+                        dummyAccounts.map(item => 
+                            <tr>
+                                <td>{item.id}</td>
+                                <td>{item.firstName + " " + item.lastName}</td>
+                                <td>{item.email}</td>
+                                <td>{item.phoneNo}</td>
+                                <td>{item.username}</td>
+                                <td>{item.department}</td>
+                                <td>{item.role}</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
         </MainContainer>
     );
 }
-
-export default Accounts;
