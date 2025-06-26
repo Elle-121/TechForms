16483accdfa29583ed3621de62d0f6cba42c4ad0
ElleDiablo <100169794+ElleDiablo@users.noreply.github.com>
2025-06-26 13:23:48 +0800

    routes folder

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Home from './pages/Home/Home';
-import Notifications from './pages/Notifications/Notifications';
-import Accounts from './pages/Accounts/Accounts';
-import Profile from './pages/Profile/Profile';
-import Header from './components/Header';
+// src/App.js
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Header />
-
-
-
-
-      
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/notifications" element={<Notifications />} />
-        <Route path="/accounts" element={<Accounts />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </Router>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ function Home(){
         <div>
             <h1>Home Page</h1>
             <p>Welcome to the home page!</p>
+            
         </div>
     );
 }
