f2cc0f44803db058a42d2d1485b44b0876157f6b
Prinz <pmjulian@up.edu.ph>
2025-06-26 15:10:21 +0800

    basic header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,69 @@
-import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
+import React from 'react'
+import { Container, Navbar, Col, Dropdown, Button } from 'react-bootstrap'
+import companylogofull from '../assets/TechFactorsIncFull.png' // <-- Make sure you have this file and path correct
+import houseicon from '../assets/HomeIcon.png'
+import notificationsicon from '../assets/NotificationsIcon.png'
+import image from '../assets/ImageIcon.png'
 
 function Header() {
-  // State to manage notifications visibility
-  const [showNotifications, setShowNotifications] = useState(false);
-  const toggleNotifications = () => {
-    setShowNotifications(!showNotifications);
-    setShowProfile(false);
-  };
 
-  // State to manage profile visibility
-  const [showProfile, setShowProfile] = useState(false);
-  const toggleProfile = () => {
-    setShowProfile(!showProfile);
-    setShowNotifications(false);
-  };
+  const logout = async () => {
+    await window.localStorage.clear()
+    window.location.href = "/";
+  }
 
   return (
-    <nav className="navbar navbar-expand navbar-dark bg-dark px-3">
-      <Link className="navbar-brand" to="/">TechFactors</Link>
-      <div className="navbar-nav">
-        <Link className="nav-link" to="/">Home</Link>
-        <Link className="nav-link" to="/accounts">Accounts</Link>
+    <Navbar expand="lg" className='top-header py-2 px-3'>
+      <Container fluid className="justify-content-between align-items-center">
+        <div className="d-flex align-items-center">
+          {/* Logo */}
+          <img src={companylogofull} alt="Logo" height="40" className="me-2" />
+        </div>
 
+        {/* Navigation and Dropdown */}
+        <div className="d-flex align-items-center gap-1">
+          <Navbar>
+            <Container>
+              <Navbar.Brand href="#home">
+                <img
+                  src={houseicon}
+                  width="20"
+                  height="20"
+                  className="d-inline-block align-center"
+                  alt="Home"
+                />
+              </Navbar.Brand>
+            </Container>
+          </Navbar>
+          <Navbar>
+            <Container>
+              <Navbar.Brand href="#notifications">
+                <img
+                  src={notificationsicon}
+                  width="20"
+                  height="20"
+                  className="d-inline-block align-center"
+                  alt="Notifications"
+                />
+              </Navbar.Brand>
+            </Container>
+          </Navbar>
 
-
-        <button onClick={toggleNotifications} className="nav-btn">Notifications</button>
-        {showNotifications && (
-          <div className="dropdown">
-            <p>You have 3 new alerts</p>
-            <p>Message from Admin</p>
-            <Link className="nav-link" to="/notifications">Notifications</Link>
-          </div>
-        )}
-
-        <button onClick={toggleProfile} className="nav-btn">Profile</button>
-        {showProfile && (
-          <div className="dropdown">
-            <p>Logged in as John Doe</p>
-            <Link to="/profile">View Profile</Link>
-            <button>Logout</button>
-          </div>
-        )}
-
-      </div>
-    </nav>
-  );
+          <Dropdown className='header-top'>
+            <Dropdown.Toggle className='user-dropdown' variant="light">
+              <img src={image} width="20" height="20" className="me-2" alt="User" />
+              Rayu Ma Masakit
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item>Profile</Dropdown.Item>
+              <Dropdown.Item onClick={logout}>Log Out</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      </Container>
+    </Navbar>
+  )
 }
 
-export default Header;
+export default Header
+
