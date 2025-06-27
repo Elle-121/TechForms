0c4ad681eb815912fb774baa930f61e929e8d4d2
JeloKuneho <llconanan@up.edu.ph>
2025-06-27 15:24:20 +0800

    Initialize Login

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -4,10 +4,12 @@ import Home from "../pages/Home/Home";
 import Profile from "../pages/Profile/Profile";
 import Accounts from "../pages/Accounts/Accounts";
 import Notifications from "../pages/Notifications/Notifications";
+import Login from "../pages/Login/Login";
 
-function AppRoutes() {
+function AppRoutes() { // TODO: Change Home Route
   return (
     <Routes>
+      <Route path="/Login" element={<Login />} />
       <Route path="/" element={<Home />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/accounts" element={<Accounts />} />
