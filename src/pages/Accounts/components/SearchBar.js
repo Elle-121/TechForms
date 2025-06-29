2466d7b77f284721f5e50efb18e421afd211211a
Angel <amhernandez@techfactors.com>
2025-06-30 07:31:13 +0800

    Add search bar on Accounts page

--- src/pages/Accounts/components/SearchBar.js
+++ src/pages/Accounts/components/SearchBar.js
@@ -1,24 +1,45 @@
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function SearchBar(){
+function SearchBarWithFilter() {
+  return (
+    <div className="d-flex align-items-center gap-3">
+      {/* Search Bar */}
+      <div
+        className="d-flex align-items-center"
+        style={{
+          backgroundColor: '#f5f5f8',
+          borderRadius: '30px',
+          padding: '5px 10px',
+        }}
+      >
+        <input
+          type="text"
+          className="form-control border-0 shadow-none"
+          placeholder="Search Request"
+          style={{
+            backgroundColor: 'transparent',
+            borderRadius: '30px',
+            paddingLeft: '10px',
+            fontSize: '14px',
+            width: '200px',
+          }}
+        />
+        <button
+          className="btn d-flex align-items-center justify-content-center"
+          style={{
+            backgroundColor: '#f7931e',
+            borderRadius: '50%',
+            padding: '6px',
+            width: '32px',
+            height: '32px',
+          }}
+        >
+          <i className="bi bi-search" style={{ color: 'white', fontSize: '16px' }}></i>
+        </button>
+      </div>
+    </div>
+  );
+}
 
-    return (
-        <Form inline>
-            <Row>
-                <Col xs="auto">
-                <Form.Control
-                    type="text"
-                    placeholder="Search"
-                    className=" mr-sm-2"
-                />
-                </Col>
-                <Col xs="auto">
-                <Button type="submit">Submit</Button>
-                </Col>
-            </Row>
-        </Form>
-    )
-};
+export default SearchBarWithFilter;
