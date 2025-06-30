26114199c055bfd2fd3e56aad6faf79bfb1abdb8
JeloKuneho <llconanan@up.edu.ph>
2025-06-30 09:57:54 +0800

    Added semantic html to Login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,34 +1,39 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MainContainer from '../../components/MainContainer';
 
-
+// TODO: Separate Logo and NavBar
 
 function Login(){
     return (
         <MainContainer>
         <div className="row h-100 m-0">
             {/* Left Side */}
-            <div className="col-md-3 col-lg-2 h-100 overflow-auto"  style={{ width: '30%', borderRight: '5px solid var(--tforange-color)' }}>
-                
+            <div className="col-md-3 col-lg-2 h-100 overflow-auto"  style={{ width: '50%', borderRight: '5px solid var(--tforange-color)' }}>
+
             </div>
-
             {/* Right Content */}
-            <div className=" p-4 h-100 overflow-auto border"  style={{ width: '70%' }}>
-                {/* Filters */}
-                {/* <Row class="border border-black">
-                    Button 1
-                    Button 2
-                    Button 3
-                    Button 4
-                </Row> */}
+            <div className="p-4 h-100 overflow-auto" style={{width: '50%',display: 'flex',flexDirection: 'column',}}>
+                <div>
+                    <h1>Welcome Back to TechForms*</h1>
+                    <h2>Manage all your Techfactor forms right here!</h2>
+                
+                    <p>Username/Email</p><p>*</p>
+                    <input value="Enter Username or Techfactors email"></input>
+                    <p>Password</p><p>*</p>
+                    <input value="Enter password"></input>
+                    <button>HIDE PASSWORD</button>
 
-                {/* Header - Requests + Searchbar + Filter */}
-                <div className="d-flex justify-content-between align-items-center pb-2" style={{ borderBottom: '2px solid #ccc' }}>
-                    <h4 className="tf-header">Requests</h4>
+                    <a href="">Forgot your password?</a>
+
+                    <button>Log In</button>
+
+
                 </div>
             </div>
+
         </div>
-        </MainContainer>
+
+         </MainContainer>
     );
 }
 
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -4,11 +4,8 @@ import Home from "../pages/Home/Home";
 import Profile from "../pages/Profile/Profile";
 import Accounts from "../pages/Accounts/Accounts";
 import Notifications from "../pages/Notifications/Notifications";
-<<<<<<< HEAD
 import Login from "../pages/Login/Login";
-=======
 import FlightRequestForm from "../pages/FlightRequestForm/FlightRequestForm";
->>>>>>> main
 
 function AppRoutes() { // TODO: Change Home Route
   return (
