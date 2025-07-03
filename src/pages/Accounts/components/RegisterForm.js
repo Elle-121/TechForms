5c23857afa19c267fb2968aec8b28492e37715ce
Angel Hernandez <amhernandez@techfactors.com>
2025-07-03 09:46:50 +0800

    Restyle form message boxes

--- src/pages/Accounts/components/RegisterForm.js
+++ src/pages/Accounts/components/RegisterForm.js
@@ -42,7 +42,7 @@ export default function RegisterForm({view, setFormView}) {
             </Modal.Header> */}
 
             <Modal.Body className="my-4">
-                <div className="tf-form-title mb-4">
+                <div className="tf-form-title mt-2 mb-4">
                     <h1 className="tf-header text-black">Register User</h1>
                 </div>
                 <div>
@@ -161,10 +161,9 @@ export default function RegisterForm({view, setFormView}) {
                         </Row>
 
                         <div className="d-flex flex-column">
-                            { !isValid && isSubmitted && <div className="form-box form-box-error mb-3 w-100 d-flex align-items-center py-3 px-4 gap-4">
+                            { !isValid && isSubmitted && <div className="form-box form-box-error mb-3 w-100 d-flex align-items-center px-4 gap-4">
                                 <i className="bi bi-exclamation-triangle-fill fs-1"/>
-                                {/* <p className='text-start m-0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus maximus sem ultricies tellus maximus, quis mollis mi suscipit. Sed efficitur sapien et sollicitudin volutpat. Maecenas sodales nulla vitae efficitur venenatis. Aliquam erat volutpat. Aenean scelerisque sagittis felis, eget viverra quam pellentesque nec. Aenean ut congue ipsum. </p> */}
-                                <p className='text-start m-0'>Error message here.</p>
+                                <p className='text-start m-0'>Please check if all required fields are filled and if all inputs are valid.</p>
                             </div> }
                             <div className="d-flex justify-content-between w-100">
                                 <button type="button" className="button-neg" onClick={handleCancel}>Cancel</button>
