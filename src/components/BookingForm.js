d224658b097824576263539efb8f1c68946c26f8
Angel Hernandez <amhernandez@techfactors.com>
2025-07-04 09:34:21 +0800

    Remove preview implementation and add input validation

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,138 +1,224 @@
 import { Modal, Form, Row, Col } from "react-bootstrap";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 export default function BookingForm({view, setFormView}) {
 
     const [preview, setPreview] = useState(false);
+    const [formValues, setFormValues] = useState();
 
-    const exitModal = () => {
-        setPreview(false);
-        setFormView(false);
-    }
+    const { register, handleSubmit, reset, formState: { errors, isValid, isSubmitted, isSubmitSuccessful } } = useForm({
+        defaultValues: {
+            departureRef: "",
+            departureCost: "",
+            departureTicket: "",
+            returnRef: "",
+            returnCost: "",
+            returnTicket: "",
+        }
+    })
 
     const home = async () => {
         window.location.href = "/";
     }
 
+    const displayValues = (values) => {
+        console.log(values);
+        setFormValues(values);
+        setFormView(false);
+        home();
+    }
+
+    useEffect(() => {
+        reset();
+    }, [view])
+
     return ( 
         <Modal show={view} size="lg" className="form-modal">
-            <Modal.Header>
-                <div className="tf-form-title">
+            <Modal.Body className="my-4">
+                <div className="tf-form-title mt-2 mb-4">
                     {
                         preview ? 
                             <h1 show={preview} className="tf-header text-black">Preview</h1> :
                             <h1 show={!preview} className="tf-header text-black">Booking Details</h1>
                     }
                 </div>
-            </Modal.Header>
+                <Form onSubmit={handleSubmit(displayValues)}>
+                    <fieldset disabled={preview}>
 
-            <Modal.Body>
-                <div>
-                    <Form>
-                        <fieldset disabled={preview}>
-    
-                            {/* Request Details */}
-                            <Row>
-                                <Col>
-                                    <Form.Group className="mb-3">
-                                        <Form.Label className='fr-form-label'>Employee Name</Form.Label>
-                                        <Form.Control disabled type="text" placeholder="Full Name"/>
-                                    </Form.Group>
-                                </Col>
-                                <Col>
-                                    <Form.Group className="mb-3">
-                                        <Form.Label className='fr-form-label'>Request ID</Form.Label>
-                                        <Form.Control disabled type="text" placeholder="TFI250630"/>
-                                    </Form.Group>
-                                </Col>
-                            </Row>
-
-                            {/* Depature Details */}
-                            <div className="tf-form-section">
-                                <h2>Departure</h2>
-                                <p>Departure Booking Details</p>
-                            </div>
-    
-                                                    <Row>
-                                {/* Booking Reference No. */}
-                                <Col>
-                                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                                        <Form.Label className='fr-form-label'>Booking Reference No.</Form.Label>
-                                        <Form.Control type="text" placeholder="Reference No." />
-                                    </Form.Group>
-                                </Col>
-                                {/* Cost */}
-                                <Col>
-                                    <Form.Group className="mb-3">
-                                        <Form.Label className='fr-form-label'>Cost</Form.Label>
-                                        <Form.Control type="number" placeholder="Php"/>
-                                    </Form.Group>
-                                </Col>
-                            </Row>
-
-                            {/* Ticket */}
-                            <Row>
-                                <Form.Group controlId="formFile" className="mb-3">
-                                    <Form.Label className='fr-form-label'>Ticket</Form.Label>
-                                    <Form.Control type="file" />
+                        {/* Request Details */}
+                        <Row>
+                            <Col>
+                                <Form.Group className="mb-3">
+                                    <Form.Label className='fr-form-label'>Employee Name</Form.Label>
+                                    <Form.Control disabled type="text" placeholder="Full Name"/>
                                 </Form.Group>
-                            </Row>
-    
-                            {/* Return Details */}
-                            <div className="tf-form-section">
-                                <h2>Return</h2>
-                                <p>Return Booking Details</p>
-                            </div>
-    
-                            <Row>
-                                {/* Booking Reference No. */}
-                                <Col>
-                                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                                        <Form.Label className='fr-form-label'>Booking Reference No.</Form.Label>
-                                        <Form.Control type="text" placeholder="Reference No." />
-                                    </Form.Group>
-                                </Col>
-                                {/* Cost */}
-                                <Col>
-                                    <Form.Group className="mb-3">
-                                        <Form.Label className='fr-form-label'>Cost</Form.Label>
-                                        <Form.Control type="number" placeholder="Php"/>
-                                    </Form.Group>
-                                </Col>
-                            </Row>
-
-                            {/* Ticket */}
-                            <Row>
-                                <Form.Group controlId="formFile" className="mb-3">
-                                    <Form.Label className='fr-form-label'>Ticket</Form.Label>
-                                    <Form.Control type="file" />
+                            </Col>
+                            <Col>
+                                <Form.Group className="mb-3">
+                                    <Form.Label className='fr-form-label'>Request ID</Form.Label>
+                                    <Form.Control disabled type="text" placeholder="TFI250630"/>
                                 </Form.Group>
-                            </Row>
-                        </fieldset>
-                    </Form>
-                </div>
+                            </Col>
+                        </Row>
+
+                        {/* Depature Details */}
+                        <div className="tf-form-section">
+                            <h2>Departure</h2>
+                            <p>Departure Booking Details</p>
+                        </div>
+
+                        <Row>
+                            {/* Booking Reference No. */}
+                            <Col>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label className='fr-form-label input-required'>Booking Reference No.</Form.Label>
+                                    <Form.Control type="text" placeholder="Reference No." className={`${errors.departureRef ? "input-invalid" : ""}`} 
+                                        {...register("departureRef", {
+                                            required: "This field is required.",
+                                            pattern : {
+                                                value: /^[a-zA-Z0-9_\-']+$/,
+                                                message: "Please enter a valid reference number."
+                                            }
+                                    })}/>
+
+                                    {errors.departureRef && 
+                                        <span className="error-msg">{errors.departureRef.message}</span>
+                                    }
+                                </Form.Group>
+                            </Col>
+                            {/* Cost */}
+                            <Col>
+                                <Form.Group className="mb-3">
+                                    <Form.Label className='fr-form-label input-required'>Cost</Form.Label>
+                                    <Form.Control type="number" placeholder="Php" className={`${errors.departureCost ? "input-invalid" : ""}`} 
+
+                                        {...register("departureCost", {
+                                            required : "This field is required.",
+                                            min: {
+                                                value: 0,
+                                                message: "PLease enter a valid cost."
+                                            }
+                                    })}/>
+                                    
+                                    {errors.departureCost && 
+                                        <span className="error-msg">{errors.departureCost.message}</span>
+                                    }
+                                </Form.Group>
+                            </Col>
+                        </Row>
+
+                        {/* Ticket */}
+                        <Row>
+                            <Form.Group controlId="formFile" className="mb-3">
+                                <Form.Label className='fr-form-label input-required'>Ticket</Form.Label>
+                                <Form.Control type="file" className={`${errors.departureTicket ? "input-invalid" : ""}`} 
+                                    {...register("departureTicket", {
+                                        required : "This field is required.",
+                                        validate: {
+                                            fileType: (files) =>
+                                                files[0]?.type === "application/pdf" || "File must be in PDF format.",
+                                            fileSize: (files) =>
+                                                files[0]?.size < 5 * 1024 * 1024 || "File must be under 5MB."
+                                        }
+                                })}/>
+                                
+                                {errors.departureTicket && 
+                                    <span className="error-msg">{errors.departureTicket.message}</span>
+                                }
+                            </Form.Group>
+                        </Row>
+
+                        {/* Return Details */}
+                        <div className="tf-form-section">
+                            <h2>Return</h2>
+                            <p>Return Booking Details</p>
+                        </div>
+
+                        <Row>
+                            {/* Booking Reference No. */}
+                            <Col>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label className='fr-form-label input-required'>Booking Reference No.</Form.Label>
+                                    <Form.Control type="text" placeholder="Reference No." className={`${errors.returnRef ? "input-invalid" : ""}`} 
+                                        {...register("returnRef", {
+                                            required : "This field is required.",
+                                            pattern : {
+                                                value: /^[a-zA-Z0-9_\-']+$/,
+                                                message: "Please enter a valid reference number."
+                                            }
+                                    })}/>
+                                    
+                                    {errors.returnRef && 
+                                        <span className="error-msg">{errors.returnRef.message}</span>
+                                    }
+                                </Form.Group>
+                            </Col>
+                            {/* Cost */}
+                            <Col>
+                                <Form.Group className="mb-3">
+                                    <Form.Label className='fr-form-label input-required'>Cost</Form.Label>
+                                    <Form.Control type="number" placeholder="Php"className={`${errors.returnCost ? "input-invalid" : ""}`} 
+                                        {...register("returnCost", {
+                                            required : "This field is required.",
+                                            min: {
+                                                value: 0,
+                                                message: "PLease enter a valid cost."
+                                            }
+                                    })}/>
+                                    
+                                    {errors.returnCost && 
+                                        <span className="error-msg">{errors.returnCost.message}</span>
+                                    }
+                                </Form.Group>
+                            </Col>
+                        </Row>
+
+                        {/* Ticket */}
+                        <Row>
+                            <Form.Group controlId="formFile" className="mb-3">
+                                <Form.Label className='fr-form-label input-required'>Ticket</Form.Label>
+                                <Form.Control type="file" className={`${errors.returnTicket ? "input-invalid" : ""}`} 
+                                    {...register("returnTicket", {
+                                        required : "This field is required.",
+                                        validate: {
+                                            fileType: (files) =>
+                                                files[0]?.type === "application/pdf" || "File must be in PDF format.",
+                                            fileSize: (files) =>
+                                                files[0]?.size < 5 * 1024 * 1024 || "File must be under 5MB."
+                                        }
+                                })}/>
+                                
+                                {errors.returnTicket && 
+                                    <span className="error-msg">{errors.returnTicket.message}</span>
+                                }
+                            </Form.Group>
+                        </Row>
+                    </fieldset>
+                    <div className="d-flex flex-column">
+                        {/* Error box */}
+                        { (!isValid && isSubmitted) && <div className="form-box form-box-error mb-3 w-100 d-flex align-items-center px-4 gap-4">
+                            <i className="bi bi-exclamation-triangle-fill fs-1"/>
+                            <p className='text-start m-0'>Please check if all required fields are filled and if all inputs are valid.</p>
+                        </div> }
+
+                        <div className="d-flex justify-content-between w-100">
+                            <button className="button-neg" onClick={() => setFormView(false)}>Cancel</button>
+                            <button type="submit" className='btn-approve'>Submit and Approve</button>
+                            {/* {
+                                preview ?
+                                    <button className="button-neg" onClick={() => setPreview(false)}>Back</button> :
+                                    <button className="button-neg" onClick={handleCancel}>Cancel</button>
+                            }
+                            {
+                                preview ?
+                                    <button type="submit" className='btn-approve' onClick={home}>Submit and Approve</button> :
+                                    <button className='button-affirm' onClick={handleNext}>Next</button>
+                            } */}
+                        </div>
+                    </div>
+                </Form>
             </Modal.Body>
-
-            <Modal.Footer className="d-flex justify-content-between">
-                {/* Error box */}
-                <div className="form-box form-box-error mb-3 w-100 d-flex align-items-center py-3 px-4 gap-4">
-                    <i className="bi bi-exclamation-triangle-fill fs-1"/>
-                    {/* <p className='text-start m-0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus maximus sem ultricies tellus maximus, quis mollis mi suscipit. Sed efficitur sapien et sollicitudin volutpat. Maecenas sodales nulla vitae efficitur venenatis. Aliquam erat volutpat. Aenean scelerisque sagittis felis, eget viverra quam pellentesque nec. Aenean ut congue ipsum. </p> */}
-                    <p className='text-start m-0'>Error message here.</p>
-                </div>
-
-                {
-                    preview ? 
-                        <button className="button-neg ms-2" onClick={() => setPreview(false)}>Back</button> :
-                        <button className="button-neg ms-2" onClick={() => setFormView(false)}>Cancel</button>
-                }
-                {
-                    preview ? 
-                        <button className='btn-approve ms-2' onClick={home}>Submit and Approve</button> :
-                        <button className='button-affirm ms-2' onClick={()=>setPreview(true)}>Next</button>
-                }
-            </Modal.Footer>
-
         </Modal>
 
     );
