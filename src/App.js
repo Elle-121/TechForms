e678ab4e77ea5d10ba4a76aecbdcc2f184a63131
LanzeTF <laconanah@techfactors.com>
2025-07-08 17:32:53 +0800

    Refactor Routing to use react-router-dom's Data mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import "./App.scss";
 
 function App() {
   return (
-    <BrowserRouter>  
+    <React.StrictMode>  
       <AppRoutes />
-    </BrowserRouter>
+    </React.StrictMode>
   );
 }
 
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,5 +1,5 @@
 // src/routes/AppRoutes.js
-import { Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Profile from "../pages/Profile/Profile";
 import Accounts from "../pages/Accounts/Accounts";
@@ -11,21 +11,63 @@ import Review from "../pages/Review/Review";
 import TwoFactorAuth from "../pages/Login/TwoFactorAuth";
 import ErrorPage from "../pages/errorPage";
 
+
+const router = createBrowserRouter([ // A JSON formatted route list
+  {
+    path: "/",
+    Component: Home,
+  },
+  {
+    path: "login",
+    Component: Login,
+  },
+  {
+    path: "reset-password",
+    Component: ResetPassword,
+  },
+  {
+    path: "two-factor-auth",
+    Component: TwoFactorAuth,
+  },
+  {
+    path: "profile",
+    Component: Profile,
+  },
+  {
+    path: "accounts",
+    Component: Accounts,
+  },
+  {
+    path: "notifications",
+    Component: Notifications,
+  },
+  {
+    path: "flight-request-form",
+    Component: FlightRequestForm,
+  },
+  {
+    path: "review",
+    Component: Review,
+  },
+]);
+
+
 function AppRoutes() { // TODO: Change Home Route
   return (
-    <Routes>
-      <Route path="login" element={<Login />} errorElement={<ErrorPage/>} />
-      <Route path="reset-password" element={<ResetPassword />} />
-      <Route path="two-factor-auth" element={<TwoFactorAuth />} />
+    <RouterProvider router={router}/>
+    // <Routes>
+    //   <Route path="login" element={<Login />} errorElement={<ErrorPage/>} />
+    //   <Route path="reset-password" element={<ResetPassword />} />
+    //   <Route path="two-factor-auth" element={<TwoFactorAuth />} />
 
-      <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/accounts" element={<Accounts />} />
-      <Route path="/notifications" element={<Notifications />} />
+    //   <Route path="/" element={<Home />} />
+    //   <Route path="/profile" element={<Profile />} />
+    //   <Route path="/accounts" element={<Accounts />} />
+    //   <Route path="/notifications" element={<Notifications />} />
       
-      <Route path="/flight-request-form" element={<FlightRequestForm />} />
-      <Route path="/review" element={<Review />} />
-    </Routes>
+    //   <Route path="/flight-request-form" element={<FlightRequestForm />} />
+    //   <Route path="/review" element={<Review />} />
+    // </Routes>
   );
 }
 
