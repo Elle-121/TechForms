bb6af3963d334aba1fda8dc702e3e042776247c9
TATSmebro <marcus.pilapil@gmail.com>
2025-07-08 11:12:09 +0800

    added date type dropdown

--- src/pages/Home/Calendar.js
+++ src/pages/Home/Calendar.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
+import { Form } from 'react-bootstrap';
 import 'react-datepicker/dist/react-datepicker.css';
 import './Calendar.scss';
 
@@ -18,41 +19,64 @@ function Calendar({setHomeDateRange, setFilterValues}) {
 
   return (
     <div className="date-time-container">
-        {/* Date and Time Column */}
-        <div className="card-container">
-            {/* Date */}
-            <div className='card card-date'>
-                <span className="fs-4 fw-semibold text-dark">{now.toLocaleDateString('en-US', { weekday: 'long' })}</span>
-                <span className="fs-1 fw-bold" style={{ color: 'var(--tforange-color)' }}>{now.getDate()}</span>
-                <span>{now.toLocaleDateString('en-US', { month: 'long', year: 'numeric' })}</span>
-            </div>
+      {/* Date and Time Column */}
+      <div className="card-container">
+          {/* Date */}
+          <div className='card card-date'>
+              <span className="fs-4 fw-semibold text-dark">{now.toLocaleDateString('en-US', { weekday: 'long' })}</span>
+              <span className="fs-1 fw-bold" style={{ color: 'var(--tforange-color)' }}>{now.getDate()}</span>
+              <span>{now.toLocaleDateString('en-US', { month: 'long', year: 'numeric' })}</span>
+          </div>
 
-            {/* Time */}
-            <div className='card card-time'>
-                <span className="fs-1 fw-bold">{time}</span>            
-                <span className="fs-3 fw-bold">{ampm}</span>
-            </div>
+          {/* Time */}
+          <div className='card card-time'>
+              <span className="fs-1 fw-bold">{time}</span>            
+              <span className="fs-3 fw-bold">{ampm}</span>
+          </div>
+      </div>
+
+      {/* Calendar Date picker */}
+      <div className="calendar-container">
+        <div>
+          <DatePicker
+              selectsRange
+              startDate={startDate}
+              endDate={endDate}
+              onChange={(update) => {
+                setDateRange(update);
+                setHomeDateRange(update);
+                setFilterValues(prev => ({
+                    ...prev,
+                    submitted_start: update[0] ? update[0].toLocaleDateString('en-CA') : "",
+                    submitted_end: update[1] ? update[1].toLocaleDateString('en-CA') : ""
+                }));
+              }}
+              isClearable={true}
+              inline
+          />
         </div>
+          
+        <div className='button-dropdown-container'>
+          
+          {/* Date Type Drowpdown */}
+          <div>
+            <Form>
+              <Form.Group>
+                <Form.Select className='calendar-dropdown'>
+                  <option value=''>Select Date Type</option>
+                  <option value='submitted'>Date Submitted</option>
+                  <option value='departure'>Departure Date</option>
+                  <option value='return'>Return Date</option>
+                  <option value='business_start'>Start of Business</option>
+                  <option value='business_end'>End of Business</option>
+                </Form.Select>
+              </Form.Group>
+            </Form>
+          </div>
 
-        {/* Calendar Date picker */}
-        <div className="calendar-container">
-            <DatePicker
-                selectsRange
-                startDate={startDate}
-                endDate={endDate}
-                onChange={(update) => {
-                  setDateRange(update);
-                  setHomeDateRange(update);
-                  setFilterValues(prev => ({
-                      ...prev,
-                      submitted_start: update[0] ? update[0].toLocaleDateString('en-CA') : "",
-                      submitted_end: update[1] ? update[1].toLocaleDateString('en-CA') : ""
-                  }));
-                }}
-                isClearable={true}
-                inline
-            />
+          {/* Clear Date Range Button */}
           {startDate && endDate && (
+          <div>
             <button
               className="calendar-reset-btn"
               type="button"
@@ -68,8 +92,10 @@ function Calendar({setHomeDateRange, setFilterValues}) {
             >
               Clear Date Range
             </button>
-          )}            
-        </div>
+          </div>
+          )}
+        </div>            
+      </div>
     </div>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -62,7 +62,7 @@ function Home() {
     <MainContainer>
         <div className="row h-100 m-0">
             {/* Left Side */}
-            <div className="p-4 col-md-3 col-lg-2 h-100 overflow-auto  " style={{width: '30%',display: 'flex', flexDirection: 'column', borderRight: '5px solid var(--tforange-color)'}}>
+            <div className="p-4 col-md-3 col-lg-2 h-100 overflow-auto" style={{width: '30%',display: 'flex', flexDirection: 'column', borderRight: '5px solid var(--tforange-color)'}}>
                 <Calendar setHomeDateRange={setHomeDateRange} setFilterValues={setFilterValues}/>
             </div>
 
