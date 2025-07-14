144fbced79f81018e8d4e4fcdab0ffc43d348fb3
LanzeTF <laconanah@techfactors.com>
2025-07-14 10:11:03 +0800

    Refactor MainContainer divs and Header + Change Logo Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import { Container, Navbar } from 'react-bootstrap';
 import './Header.scss';
 import Navigation from './Navigation';
+import { Link } from 'react-router-dom';
 
 // Assets
 import companyLogoFull from '../assets/TechFactorsIncFull.png' 
@@ -12,9 +13,9 @@ function Header({ navVisible = true }) {
   return (
     <Navbar expand="lg" className="top-header py-2 px-3 bg-white" style={{ boxShadow: '0 2px 8px rgba(0,0,0,0.08)' }}>
       <Container fluid className="justify-content-between align-items-center">
-        <div className="d-flex align-items-center">
+        <Link to="/" className="d-flex align-items-center">
           <img src={companyLogoFull} alt="Logo" height="50" className="me-2" />
-        </div>
+        </Link>
 
         {navVisible && <Navigation/>}
       </Container>
