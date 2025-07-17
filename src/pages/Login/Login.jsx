68db7d8fd65942ba94725c833fb802a1ba39b5aa
JeloKuneho <llconanan@up.edu.ph>
2025-07-17 11:17:25 +0800

    Refactored Login Page to use new CSS Module

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import MainContainer from '../../components/MainContainer';
 import { Link, useNavigate } from 'react-router-dom';
 import loginImage from "../../assets/LoginImage.jpeg"
+import styles from "./login.module.scss";
 
 // TODO: https://www.reddit.com/r/webdev/comments/nr9rso/how_to_validate_forms_properly_some_useful_dos/
 
@@ -18,8 +19,9 @@ function Login(){
             <div className="col-md-3 col-lg-2"
                 style={{ width: '50%',
                         borderRight: '5px solid var(--tforange-color)' }}>
-                <figure className="login-figure">
-                    <img src={loginImage} className="login-figure__image"
+                <h1 style={{textAlign: 'center', fontWeight: 'bold'}}>Made By</h1>
+                <figure className={styles['login-figure']}>
+                    <img src={loginImage} className={styles['login-figure__image']}
                     alt="TechFactor Interns"/>
                 </figure>
             </div>
@@ -30,37 +32,36 @@ function Login(){
                     justifyContent: 'center',
                     position: 'relative',
                     top: '50px'}}>
-                <div className="login-container">
-                    <header className="login-form-header">
-                        <h1 className="login-text--center login-text--bold">
+                <div className={styles["login-container"]}>
+                    <header className={styles["login-header"]}>
+                        <h1 className={styles["login-header__h1"]}>
                             Welcome Back to
-                            <strong className="login-text--orange"> TechForms*</strong></h1>
-                        <p className="login-text--center">
+                            <strong className={styles["login-header__strong"]}> TechForms*</strong></h1>
+                        <p>
                             Manage all your TechFactors forms right here!</p>
                     </header>
 
-                    <form action='' method='get' className='login-form'> {/* TODO fill in action and method attributes */}
-                        <div className='form-row'>
+                    <form action='' method='get' className={styles['login-form']}> {/* TODO fill in action and method attributes */}
+                        <div className={styles['form-row']}>
                             <label for='username'>Username / TechFactors Email</label>
                             <input id='username' name='username' type='text'
                                 placeholder="Enter Username or TechFactors email"
-                                className="form-row__input-text"/>
+                                className={styles["form-row__input-text"]}/>
                             {/* TODO Figure out input type of Username/Email*/}
                         </div>
 
-                        <div className='form-row'>
+                        <div className={styles['form-row']}>
                             <label for='password'>Password</label>
                             <input id='password' name='password' type='password'
                                 placeholder="Enter password"
-                                className="form-row__input-text"/>
+                                className={styles["form-row__input-text"]}/>
                             {/* TODO Add Unhide Password https://www.wmcsoft.com/blog/how-to-implement-a-password-reveal*/}
-                            <Link to="/reset-password" className="login-text--small">
+                            <Link to="/reset-password" className={styles['form-row__text--small']}>
                                 Forgot your password?</Link>
                         </div>
 
-                        <div className='button-row'>
-                            <button type="submit" className="button-row__button
-                                button-row__button--affirm"
+                        <div className={styles['button-row']}>
+                            <button type="submit" className={styles["button-row__button--affirm"]}
                                 onClick={handleSubmit}>Log In</button>
                         </div>
 
