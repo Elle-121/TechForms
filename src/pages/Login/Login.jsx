f9315f9c48353196f06836f8f524e6d85bec018e
JeloKuneho <llconanan@up.edu.ph>
2025-07-18 12:59:47 +0800

    Add rough frontend validation to login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,52 @@
-import MainContainer from '../../components/MainContainer';
 import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { z } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
+
+import MainContainer from '../../components/MainContainer';
 import loginImage from "../../assets/LoginImage.jpeg"
 import styles from "./login.module.scss";
 
 // TODO: https://www.reddit.com/r/webdev/comments/nr9rso/how_to_validate_forms_properly_some_useful_dos/
+// TODO: Connect validation to backend
+
+const schemaValidator = z.object({
+    username: z.email(
+        {
+            error: "Oops this is not an email address",
+        }
+    ),
+    password: z.string(
+        {
+            error: "This doesn't seem right..."
+        }).min(1,
+        {
+            error: "It's empty!"
+        }
+        ),
+});
 
 function Login(){
     let navigate = useNavigate();
-    
-    const handleSubmit = () => {
-        navigate("/");
+    const { register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+     } = useForm({
+        resolver: zodResolver(schemaValidator),
+     });
 
+        
+    const onSubmit = (data) => {
+        try {
+            console.log(errors)
+    
+            navigate("/");
+        }
+        catch (error) {
+            
+        }
     }
 
     return (
@@ -41,29 +77,48 @@ function Login(){
                             Manage all your TechFactors forms right here!</p>
                     </header>
 
-                    <form action='' method='get' className={styles['login-form']}> {/* TODO fill in action and method attributes */}
+                    <form action='' method='get' className={styles['login-form']}
+                        onSubmit={handleSubmit(onSubmit)}> {/* TODO fill in action and method attributes */}
                         <div className={styles['form-row']}>
-                            <label for='username'>Username / TechFactors Email</label>
+                            <label htmlFor='username'>Username / TechFactors Email
+                                {errors.username && (
+                                <span className={styles['error-text']}> *</span>
+                                )}
+                            </label>
                             <input id='username' name='username' type='text'
                                 placeholder="Enter Username or TechFactors email"
-                                className={styles["form-row__input-text"]}/>
-                            {/* TODO Figure out input type of Username/Email*/}
+                                className={styles["form-row__input-text"]}
+                                {...register("username")}/>
+                            {errors.username && (<p>
+                            <FontAwesomeIcon icon={faTriangleExclamation} />
+                            <span className={styles['error-text']}>&nbsp;{errors.username.message}</span>
+                            </p>)} 
                         </div>
-
                         <div className={styles['form-row']}>
-                            <label for='password'>Password</label>
+                            <label htmlFor='password'>Password
+                                {errors.password && (
+                                <span className={styles['error-text']}> *</span>
+                                )}
+                            </label>
                             <input id='password' name='password' type='password'
                                 placeholder="Enter password"
-                                className={styles["form-row__input-text"]}/>
-                            {/* TODO Add Unhide Password https://www.wmcsoft.com/blog/how-to-implement-a-password-reveal*/}
-                            <Link to="/reset-password" className={styles['form-row__text--small']}>
+                                className={styles["form-row__input-text"]}
+                                {...register("password")}/>
+                            {errors.password && (<p>
+                            <FontAwesomeIcon icon={faTriangleExclamation} />
+                            <span className={styles['error-text']}>&nbsp;{errors.password.message}</span>
+                            </p>)}                             {/* TODO Add Unhide Password https://www.wmcsoft.com/blog/how-to-implement-a-password-reveal*/}
+                            <Link to="/reset-password" className={styles["login-form__link"]}>
                                 Forgot your password?</Link>
                         </div>
 
                         <div className={styles['button-row']}>
                             <button type="submit" className={styles["button-row__button--affirm"]}
-                                onClick={handleSubmit}>Log In</button>
+                            disabled={isSubmitting}>Log In</button>
                         </div>
+                        {errors.root && (
+                            <div>{errors.root.message}</div>
+                        )}
 
                         
                     </form>
