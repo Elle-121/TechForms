10e5596a17a5f289687ff2ee84ac40f645557782
JeloKuneho <llconanan@up.edu.ph>
2025-07-29 11:05:43 +0800

    Add validation to Reset Password Page

--- src/pages/Login/ResetPassword.jsx
+++ src/pages/Login/ResetPassword.jsx
@@ -1,17 +1,158 @@
-import MainContainer from '../../components/MainContainer';
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { z } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+import MainContainer from '../../components/MainContainer';
 import styles from "./login.module.scss";
 
 
-// TODO fix CSS organization (i.e., class names)
-
-function ResetPassword(){
-    let navigate = useNavigate();
-
-    const handleSubmit = () => {
-        navigate("/two-factor-auth");
-
+const schemaValidator = z.object({
+    username: 
+        z.union([ // First Checks if Username. If @ is included, starts checking full email regex
+                z.string({error: "TypeError: Not a string"})
+                    .refine((username) => 
+                        !z.string().includes("@").safeParse(username).success, 
+                        { error: "Oops this is not a valid email address"}) // If Error, UX assumes user is trying to input an email
+                    .nonempty({ error: "It's empty! "}),
+                z.email({ error: "Oops this is not a valid email address"}) // Checks full email regex
+                    .nonempty({ error: "It's empty! "})
+        ])
+    ,
+    newPassword:
+        z.string({error: "TypeError: Not a string"})
+            .nonempty({error: "It's empty!"}),
+    confirmPassword: 
+        z.string({error: "TypeError: Not a string"})
+            .nonempty({error: "It's empty!"})
+}).refine((data) => 
+    data.newPassword === data.confirmPassword,
+    { 
+        error: "Oh no! Passwords don't match :(",
+        path: ["confirmPassword"],
     }
+);
+
+
+function FormContainer() {
+    let [passwordHidden, setPasswordHidden] = useState(true);
+    let navigate = useNavigate();
+    const { register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+        } = useForm({
+        resolver: zodResolver(schemaValidator),
+    });
+
+
+    const onSubmit = (data) => {
+        try {
+            console.log(errors);
+            navigate("/two-factor-auth");
+        }
+        catch (error) {
+            console.log(errors);
+        }
+    }
+
+    const handlePasswordReveal = () => {
+        setPasswordHidden(!passwordHidden); 
+    }
+
+    return (
+    <Form className={styles['login-form']} onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group className={styles['form-row']} controlId="formUsername">
+            <Form.Label>
+                Username / TechFactors Email
+                {errors.username && (
+                     <span className={styles['error-text']}> *</span>
+                 )}
+            </Form.Label>
+            <Form.Control type="text" 
+                placeholder="Enter Username or TechFactors Email"
+                className={styles["form-row__input-text"]}
+                {...register("username")}/>
+            {/* FIX Form Control Sizing using: https://developer.mozilla.org/en-US/docs/Web/CSS/field-sizing#browser_compatibility */}
+            <Form.Text style={{ visibility: errors.username ? 'visible' : 'hidden' }}>
+                 <FontAwesomeIcon icon={faTriangleExclamation} />
+                 <span className={styles['error-text']}>&nbsp;{errors.username?.message}</span>
+            </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formNewPassword" className={styles['form-row']} >
+            <Form.Label>
+                New Password
+                {errors.newPassword && (
+                 <span className={styles['error-text']}> *</span>
+                 )}
+            </Form.Label>
+            <InputGroup>
+                <Form.Control type={passwordHidden ? "password" : "text"}
+                    placeholder="Enter New Password"
+                    className={styles["form-row__input-text"]}
+                    {...register("newPassword")}/>
+                <Button type="button" className={styles['form-row__input-button']}
+                    onClick={handlePasswordReveal}>
+                    {passwordHidden ?
+                        <FontAwesomeIcon icon={faEye} /> : 
+                        <FontAwesomeIcon icon={faEyeSlash} />
+                    }
+                </Button>
+            </InputGroup>
+            <Form.Text style={{ visibility: errors.newPassword ? 'visible' : 'hidden' }}>
+                 <FontAwesomeIcon icon={faTriangleExclamation} />
+                 <span className={styles['error-text']}>&nbsp;{errors.newPassword?.message}</span>
+             </Form.Text>
+        </Form.Group>
+
+
+        <Form.Group controlId="formConfirmPassword" className={styles['form-row']} >
+            <Form.Label>
+                Confirm Password
+                {errors.confirmPassword && (
+                 <span className={styles['error-text']}> *</span>
+                 )}
+            </Form.Label>
+            <InputGroup>
+                <Form.Control type={passwordHidden ? "password" : "text"}
+                    placeholder="Confirm New Password"
+                    className={styles["form-row__input-text"]}
+                    {...register("confirmPassword")}/>
+                <Button type="button" className={styles['form-row__input-button']}
+                    onClick={handlePasswordReveal}>
+                    {passwordHidden ?
+                        <FontAwesomeIcon icon={faEye} /> : 
+                        <FontAwesomeIcon icon={faEyeSlash} />
+                    }
+                </Button>
+            </InputGroup>
+            <Form.Text style={{ visibility: errors.confirmPassword ? 'visible' : 'hidden' }}>
+                 <FontAwesomeIcon icon={faTriangleExclamation} />
+                 <span className={styles['error-text']}>&nbsp;{errors.confirmPassword?.message}</span>
+             </Form.Text>
+        </Form.Group>
+
+         <Form.Group controlId="formSubmission" className={styles['button-row']} >
+            <Link to="/login" className={styles["button-row__button--negate"]}>Back</Link>
+            <Button type="submit" className={styles["button-row__button--affirm"]} disabled={isSubmitting}>Next</Button>
+         </Form.Group>
+    </Form>
+    );
+
+}
+
+
+export default function ResetPassword(){
+
 
     return (
         <MainContainer navVisible={false}>
@@ -27,37 +168,8 @@ function ResetPassword(){
                         <h1 className={styles["login-header__h1"]}>Reset your password</h1>
                     </header>
 
-                    <form action='' method='get' className={styles['login-form']}> {/* TODO fill in action and method attributes */}
-                        <div className={styles['form-row']}>
-                            <label htmlFor='username'>Username / TechFactors Email</label>
-                            <input id='username' name='username' type='text'
-                                placeholder="Enter Username or TechFactors email"
-                                className={styles["form-row__input-text"]}/>
-                            {/* TODO Figure out input type of Username/Email*/}
-                        </div>
+                    <FormContainer />
 
-                        <div className={styles['form-row']}>
-                            <label htmlFor='new-password'>New Password</label>
-                            <input id='new-password' name='new-password' type='password'
-                                placeholder="Enter new password"
-                                className={styles["form-row__input-text"]}/>
-                            {/* TODO Add Unhide Password https://www.wmcsoft.com/blog/how-to-implement-a-password-reveal*/}
-                        </div>
-
-                        <div className={styles['form-row']}>
-                            <label htmlFor='confirm-password'>Confirm Password</label>
-                            <input id='confirm-password' name='confirm-password' type='password'
-                                placeholder="Confirm new password"
-                                className={styles["form-row__input-text"]}/>
-                            {/* TODO Add Unhide Password https://www.wmcsoft.com/blog/how-to-implement-a-password-reveal*/}
-                        </div>
-
-                        <div className={styles['button-row']}>
-                            <Link to="/login" className={styles["button-row__button--negate"]}>Back</Link>
-                            <button type="submit" className={styles["button-row__button--affirm"]} onClick={handleSubmit}>Next</button>
-                        </div>
-
-                    </form>
                 </div>
             </div>
 
@@ -66,6 +178,3 @@ function ResetPassword(){
          </MainContainer>
     );
 }
-
-
-export default ResetPassword
